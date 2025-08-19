2edecdd03e8ef87d12defeca99c5520c0a32fd75
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-08-19 04:05:38 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {setupGlobalState} from './state/data/GlobalStore'
 export * from './state/data/SessionStorageUtils'
 
 export {BaseDynamicComponent} from './components/BaseDynamicComponent'
+export {BaseTemplateDynamicComponent} from './components/BaseTemplateDynamicComponent'
 export {BaseThunkAction} from './state/update/BaseThunkAction'
 export * from './state/update/BaseThunk'
 export type {EventHandlerThunkConfig} from './state/update/event/types/EventHandlerThunkConfig.js'
